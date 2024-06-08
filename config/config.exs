e0f504650cf00c516d0dc41bfbe69b13ab37fd31
samrose <samuel.rose@gmail.com>
2024-06-08 15:57:06 -0400

    chore: enabled by default for now

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :multivac, Multivac.MultivacLibcluster, enabled: false
+config :multivac, Multivac.MultivacLibcluster, enabled: true
 config :logger, :console,
   format: "[$level][node:$node] $message\n", metadata: [:node]
 
