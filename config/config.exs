501ae8d0953f8619e0f6bcfbaa8c48be6e88119a
Sam Rose <samuel@supabase.io>
2024-12-29 12:54:14 -0500

    chore

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,9 @@ config :multivac_agent, MultivacAgent.Repo,
   database: System.get_env("PGDATABASE", "postgres"),
   username: System.get_env("PGUSER", "postgres"),
   password: System.get_env("PGPASSWORD"),
-  hostname: System.get_env("PGHOST", "db.lhyicfpzonckkfgmmmrl.supabase.co"),
+  hostname: System.get_env("PGHOST"),
   port: String.to_integer(System.get_env("PGPORT", "5432"))
-  
+
 config :multivac_agent, ecto_repos: [MultivacAgent.Repo]
 
 # Import environment specific config. This must remain at the bottom
